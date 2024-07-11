1a33d6a8cc0ffd415e4d39c9962f28579840414b
githubdess <dess_81@mail.ru>
2024-07-11 12:42:55 +0300

    Add files via upload

--- module_2_3.py
+++ module_2_3.py
@@ -1,21 +1,19 @@
-print('Первый вариант решения: ')
-while True:
-    my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-    my_list = [i for i in my_list if i > 0]
-    for i in my_list:
-        print(i)
-    break
-
-print('Второй вариант решения: ')
+# print('Первый вариант решения: ')
+# while True:
+#     my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+#     my_list = [i for i in my_list if i > 0]
+#     for i in my_list:
+#         print(i)
+#     break
+#
+# print('Второй вариант решения: ')
 my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 i = 0
 while i < len(my_list):
-    print(my_list[i])
-    i += 1
-    if my_list[i] > 0:
-        print(my_list[i])
-        i += 1
     if my_list[i] == 0:
         i += 1
     if my_list[i] < 0:
         break
+    if my_list[i] > 0:
+        print(my_list[i])
+        i += 1
