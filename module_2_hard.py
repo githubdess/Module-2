6e7a48b2d8c755c175c40b4e73d1875854009ba1
githubdess <dess_81@mail.ru>
2024-08-01 11:44:08 +0300

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,13 @@
+print('Введите число: ')
+
+
+def password(number):
+    pass_ = ''
+    for i in range(1, number):
+        for j in range(i + 1, number):
+            if number % (i + j) == 0:
+                pass_ += str(i) + str(j)
+    return pass_
+
+
+print(password(int(input())))
